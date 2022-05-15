745996d94b81c688f0281939f24328d0cd747836
Ronlin1 <ronlinx6@gmail.com>
2022-05-15 23:07:40 +0300

    test usecase

--- Test/test.py
+++ Test/test.py
@@ -2,4 +2,9 @@ from urlToHTML import urltohtml
 
 initiate = urltohtml.Blog()
 save_blog = initiate.save("https://blog.octachart.com/", "test_file")
+
 print(save_blog)
+
+# You can create multiple URLs to loop from and saving them to 
+# a different name every loop;
+
